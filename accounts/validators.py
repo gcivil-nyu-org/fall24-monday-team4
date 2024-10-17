3158e2d6059fe8f0158042e74c86351c0a9352e5
Jeffrey Wong <70869230+jeffwong97@users.noreply.github.com>
2024-10-16 21:00:59 -0400

    Fix readme

--- accounts/validators.py
+++ accounts/validators.py
@@ -1,6 +1,7 @@
 from django.core.validators import EmailValidator
 from django.utils.deconstruct import deconstructible
 
+# Function to validate only nyu.edu email addresses for sign up
 @deconstructible
 class NyuEmailValidator(EmailValidator):
     def validate_domain_part(self, domain_part):
