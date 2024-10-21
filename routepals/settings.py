49d14f34e37d73517ebcccb37247174e205e1167
Samara Augustin <samaraaugust123@gmail.com>
2024-10-21 14:36:54 -0400

    Added user profile page

--- routepals/settings.py
+++ routepals/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -39,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'accounts',
     'bootstrap5',
-    'user_profile'
+    'user_profile',
+    'fontawesomefree'
 ]
 
 MIDDLEWARE = [
@@ -101,6 +103,40 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '{levelname} {asctime} {module} {message}',
+            'style': '{',
+        },
+        'simple': {
+            'format': '{levelname} {message}',
+            'style': '{',
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'WARNING',
+            'propagate': True,
+        },
+        'user_profile': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
+    },
+}
 
 
 # Internationalization
--- user_profile/views.py
+++ user_profile/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
-# from .models import Profile
+from .models import UserProfile
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+import logging
 
+logger = logging.getLogger(__name__)
+
+@login_required
 def profile_view(request):
-    # profile = Profile.objects.get(user=request.user)
-    return render(request, 'profile.html')
+    profile = get_object_or_404(UserProfile, user=request.user)
+    # logger.debug('This is a debug message', profile.user.first_name, profile.user.last_name, profile.user.email)
+    return render(request, 'profile.html', {'profile': profile})
