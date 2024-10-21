b5a6bd6c106c1b9dee0d1120fc74168c9ba226c5
Samara Augustin <samaraaugust123@gmail.com>
2024-10-21 16:42:30 -0400

    add update functionality for bio

--- user_profile/views.py
+++ user_profile/views.py
@@ -1,5 +1,5 @@
 from .models import UserProfile
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 import logging
 
@@ -8,5 +8,11 @@ logger = logging.getLogger(__name__)
 @login_required
 def profile_view(request):
     profile = get_object_or_404(UserProfile, user=request.user)
-    # logger.debug('This is a debug message', profile.user.first_name, profile.user.last_name, profile.user.email)
+
+    if request.method == 'POST':
+        new_bio = request.POST.get('bio')
+        profile.bio = new_bio
+        profile.save()
+        return redirect('profile')
+
     return render(request, 'profile.html', {'profile': profile})
