69069ad2d2a2b85258c74d4f8b52f5f0cfdbb2bc
Samara Augustin <samaraaugust123@gmail.com>
2024-10-27 20:43:04 -0400

    added template and function to send welcome email

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,19 @@ from django.contrib.auth.forms import *
 from django.contrib.auth import login
 from django.shortcuts import render, redirect
 from .forms import SignUpForm
+from django.conf import settings
+from django.core.mail import send_mail, EmailMessage
+from django.template.loader import render_to_string
+
+def welcomeEmail(user):
+    subject = "Welcome to Our RoutePals!"
+    html_message = render_to_string('emails/welcome_email.html', {
+        'website_link': 'http://localhost:8000', #change this link to be specific to the real link
+        'username': user.username
+    })
+    email = EmailMessage(subject, html_message, settings.DEFAULT_FROM_EMAIL, [user.email])
+    email.content_subtype = "html"
+    email.send()
 
 # Post user account sign up form
 def SignUp(request):
@@ -10,6 +23,7 @@ def SignUp(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
+            welcomeEmail(user)
             return redirect('home')
     else:
         form = SignUpForm()
