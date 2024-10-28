c65e4f9f39c85de3f7cd26a753b38096730d7d9a
Jeffrey Wong <70869230+jeffwong97@users.noreply.github.com>
2024-10-28 01:52:17 -0400

    Update settings.py
    Remove local GDAL paths

--- routepals/settings.py
+++ routepals/settings.py
@@ -16,9 +16,6 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-GDAL_LIBRARY_PATH = '/opt/homebrew/opt/gdal/lib/libgdal.dylib'
-GEOS_LIBRARY_PATH = '/opt/homebrew/opt/geos/lib/libgeos_c.dylib'
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
