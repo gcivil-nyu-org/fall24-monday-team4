cae8c141c6186b986c15c4c4e06392fd91e239ba
shashankdatta <shashankdattabezgum@gmail.com>
2024-10-28 21:22:15 -0400

    Fixed Profile Issues

--- user_profile/apps.py
+++ user_profile/apps.py
@@ -1,9 +1,7 @@
 from django.apps import AppConfig
 
-
 class UserProfileConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "user_profile"
-
-    # def ready(self):
-    #     import user_profile.signals
+    def ready(self):
+        import user_profile.signals  # This imports and connects your signals
--- user_profile/models.py
+++ user_profile/models.py
@@ -7,5 +7,5 @@ class UserProfile(models.Model):
     bio = models.TextField(blank=True, null=True)
     photo_key = models.CharField(max_length=255, blank=True, null=True)
 
-    # def __str__(self):
-    #     return f"Profile for {self.user.username} ({self.user.email})"
+    def __str__(self):
+        return f"Profile for {self.user.username} ({self.user.email})"
