331afbb245aa991594ceb80641dbddabfc1d6650
shashankdatta <shashankdattabezgum@gmail.com>
2024-10-28 20:54:16 -0400

    Fixed Delete Admin Issues

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("profile/", views.profile_view, name="profile"),
+    path("myprofile/", views.profile_view, name="profile"),
 ]
