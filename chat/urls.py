eb9a58292ff93ae9b2ac24435632cddbee634936
Idanlau <lauidan31@gmail.com>
2024-11-07 22:23:53 -0500

    fixed chat

--- chat/urls.py
+++ chat/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.index, name="chat"),
     path("chatrooms/", views.chat_room, name="chat_room"),
     path("chatrooms/<pk>/", views.chat_room, name="chat_room"),
     path("create_chat_room/", views.create_chat_room, name="create_chat_room"),
