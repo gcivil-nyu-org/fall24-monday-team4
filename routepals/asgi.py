d5272864ecf843f2602582fc6db5abbc13566612
Idanlau <lauidan31@gmail.com>
2024-11-08 00:31:32 -0500

    revert to rouepals.settings

--- routepals/asgi.py
+++ routepals/asgi.py
@@ -5,7 +5,7 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 
 from chat import routing
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ChatApp.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "routepals.settings")
 
 application = ProtocolTypeRouter(
     {
