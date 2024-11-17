431280af19709b228afce5575e94f96d8226427e
Jeffrey Wong <70869230+jeffwong97@users.noreply.github.com>
2024-11-17 15:17:02 -0500

    fix migrations

--- accounts/migrations/0003_userdocument_description.py
+++ accounts/migrations/0003_userdocument_description.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("accounts", "0002_userdocument"),
+        ("accounts", "0001_initial"),
     ]
 
     operations = [
