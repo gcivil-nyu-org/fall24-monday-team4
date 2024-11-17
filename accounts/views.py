2cdb2846418c2bbebb3698d01a340e61881bdb92
Samara Augustin <samaraaugust123@gmail.com>
2024-11-17 09:39:47 -0500

    deleted files, added logic to delete files and fixed layout and code syntax for uploading documents.

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.forms import *
 from django.contrib.auth import login
 from django.contrib.auth.views import PasswordChangeView, PasswordResetView
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import *
 from django.conf import settings
 from django.core.mail import EmailMessage
@@ -9,10 +9,12 @@ from django.template.loader import render_to_string
 from django.urls import reverse_lazy, reverse
 from django.contrib.messages.views import SuccessMessageMixin
 from .models import UserDocument
-from utils.s3_utils import upload_file_to_s3
+from utils.s3_utils import upload_file_to_s3, generate_presigned_url, delete_file_from_s3
 import uuid
 from django.http import JsonResponse
-
+from django.db.models import Q, Count
+from django.contrib.auth.models import User
+import json
 
 def WelcomeEmail(user):
     subject = "Welcome to RoutePals!"
@@ -100,6 +102,9 @@ def AdminCreation(request):
 
 def uploaded_documents_view(request):
     documents = UserDocument.objects.filter(user=request.user, deleted_at__isnull=True)
+    for document in documents:
+        document.documentUrl = generate_presigned_url(document.s3_key)
+
     return render(
         request,
         "documents/user_document_list.html",
@@ -116,7 +121,6 @@ def upload_document_modal(request):
 def upload_document(request):
     if request.method == "POST" and request.FILES.get("document"):
         document = request.FILES["document"]
-        name = request.POST.get("fileName")
         description = request.POST.get("fileDescription")
         user = request.user
         unique_key = str(uuid.uuid4())
@@ -124,18 +128,70 @@ def upload_document(request):
         try:
             s3_url = upload_file_to_s3(document, unique_key)
 
-            user_document = UserDocument.objects.create(
+            UserDocument.objects.create(
                 user=user,
-                filename=name,
+                filename=document.name,
                 description=description,
                 s3_key=unique_key,
                 file_type=document.content_type,
             )
-            # Delete Document thats being passed
+
             return JsonResponse(
-                {"success": True, "url": s3_url, "document": user_document}
+                {"success": True, "url": s3_url}
             )
         except Exception as e:
             return JsonResponse({"success": False, "error": str(e)})
 
     return JsonResponse({"success": False, "error": "Invalid request"})
+
+def documents_list(request):
+    active_users = User.objects.filter(
+        Q(documents__deleted_at__isnull=True) & Q(documents__s3_key__isnull=False)
+    ).distinct()
+
+    user_documents = []
+
+    for user in active_users:
+        documents = user.documents.filter(deleted_at__isnull=True)
+
+        pending_count = user.documents.filter(status__id=1, deleted_at__isnull=True).count()
+
+        document_data = [
+            {
+                'filename': document.filename,
+                'file_type': document.file_type,
+                'created_at': document.created_at,
+                'status': document.status,
+                'description': document.description,
+                'document_url': generate_presigned_url(document.s3_key),
+            }
+            for document in documents
+        ]
+
+        if document_data:
+            user_documents.append({
+                'user': user,
+                'documents': document_data,
+                'pending_count': pending_count,
+            })
+
+    return render(request, 'documents/documents_list.html', {
+        'user_documents': user_documents
+    })
+
+def delete_document(request):
+    try:
+        data = json.loads(request.body)
+        document_id = data.get('document_id')
+
+        if document_id is None:
+            return JsonResponse({'success': False, 'error': 'No document with this id found.'}, status=400)
+
+        document = get_object_or_404(UserDocument, id=document_id)
+        delete_file_from_s3(document.s3_key)
+        document.delete()
+
+        return JsonResponse({'success': True, 'message': 'Document has been successfully deleted.'})
+
+    except Exception as e:
+        return JsonResponse({'success': False, 'error': str(e)}, status=500)
